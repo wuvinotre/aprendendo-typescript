b34a4b6d20f4b1d2f6a9f0ac549633e41df3b812
Ewerton <ewerton.viniciusturco@gmail.com>
2022-02-16 11:36:06 -0300

    first commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,16 @@
+export class Negociacao {
+  constructor(
+    private readonly _data: Date,
+    private readonly quantidade: number, 
+    private readonly valor: number
+    ) {}
+
+  get volume(): number {
+    return this.quantidade * this.valor;
+  }
+
+  get data(): Date{
+    const data = new Date(this._data.getTime());
+    return data;
+  }
+}
